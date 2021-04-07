b5bd00a5b5f50bd9d1d97b9e3a1ec3b58d08c53f
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-07 14:40:16 -0500

    countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var highScoreEl = document.querySelector("#high-score");
 var pageEl = document.querySelector("main");
+var timerEl = document.querySelector("#timer-count");
 var questionNumber = 0;
+var remainingTime = 10;
 
 var questions = [{
     question : "Common data types do not include:",
@@ -18,6 +20,12 @@ var clearMain = function(){
     while(pageEl.firstChild){
         pageEl.removeChild(pageEl.firstChild);
     }
+}
+var mainScreen = function(){
+    clearMain();
+    var maindisplay = document.createElement('div');
+    maindisplay.innerHTML = '<h1>Coding Quiz Challenge</h1><p>Try to answer the following questions.</p><p>Once you begin, you will have 60 seconds to complete the quiz.</p><p>Wrong answers will deduct 10 seconds from you time.</p><p>Good Luck!!</p><button class="btn" id="start">Begin Quiz</button>';
+    pageEl.appendChild(maindisplay);
 }
 var displayQuestion = function(oneQuestion){
     clearMain();
@@ -40,11 +48,19 @@ var displayQuestion = function(oneQuestion){
     questionNumber++;
 }
 
+var endScreen = function (){
+    clearMain();
+    var closingStatement = document.createElement("h1");
+    closingStatement.textContent = "The Quiz is over";
+    pageEl.appendChild(closingStatement);
+}
+
 var clickDecision = function(event){
     console.log ("button clicked");
     var targetEl = event.target;
     if (targetEl.matches("#start")){
         displayQuestion(questions[questionNumber]);
+        countDown();
     } else {
         var answerChosen = parseInt(targetEl.getAttribute("data-answer-id"));
     console.log(answerChosen,questions[questionNumber-1].correctAnswer)
@@ -54,10 +70,7 @@ var clickDecision = function(event){
         console.log("wrong");
     }
     if (questionNumber>=questions.length){
-        clearMain();
-        var closingStatement = document.createElement("h1");
-        closingStatement.textContent = "The Quiz is over";
-        pageEl.appendChild(closingStatement);
+        endScreen();
     } else if (targetEl.matches(".answer")){
     
     displayQuestion(questions[questionNumber]);
@@ -69,7 +82,17 @@ var highScore = function(event){
     highScoreTitle.textContent = "High Scores";
     pageEl.appendChild(highScoreTitle);
 }
-  
+var countDown = function(){
+var counter = setInterval(function(){
+    timerEl.textContent = remainingTime;
+    remainingTime--;
+    if (remainingTime === 0){
+        endScreen();
+        clearInterval(counter);
+    }
+},1000);
+}
+mainScreen();  
 pageEl.addEventListener('click',clickDecision);
 highScoreEl.addEventListener('click',highScore);
 
