c63e4c57928d4cb211d52a501d83e8b1b663a36d
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-07 15:35:08 -0500

    added final screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var pageEl = document.querySelector("main");
 var timerEl = document.querySelector("#timer-count");
 var questionNumber = 0;
 var remainingTime = 10;
+var numberCorrect = 0;
+var counter = 0;
 
 var questions = [{
     question : "Common data types do not include:",
@@ -23,6 +25,9 @@ var clearMain = function(){
 }
 var mainScreen = function(){
     clearMain();
+    remainingTime = 60;
+    questionNumber = 0;
+    numberCorrect = 0;
     var maindisplay = document.createElement('div');
     maindisplay.innerHTML = '<h1>Coding Quiz Challenge</h1><p>Try to answer the following questions.</p><p>Once you begin, you will have 60 seconds to complete the quiz.</p><p>Wrong answers will deduct 10 seconds from you time.</p><p>Good Luck!!</p><button class="btn" id="start">Begin Quiz</button>';
     pageEl.appendChild(maindisplay);
@@ -49,28 +54,45 @@ var displayQuestion = function(oneQuestion){
 }
 
 var endScreen = function (){
+    clearInterval(counter);
+    remainingTime = Math.max(remainingTime,0);
+    console.log(remainingTime);
+    timerEl.textContent = remainingTime;
     clearMain();
-    var closingStatement = document.createElement("h1");
-    closingStatement.textContent = "The Quiz is over";
-    pageEl.appendChild(closingStatement);
+    var finalScore = 10*numberCorrect + remainingTime;
+    var closingTitle = document.createElement("div");
+    closingTitle.innerHTML = "<h1>Finished</h1><p>Each correct answer was worth 10 points.  You had " + numberCorrect + " answers correct and time bonus of " + remainingTime + ".</p><p>Your total score was " + finalScore + ".</p>";
+    pageEl.appendChild(closingTitle);
+    var mainButton = document.createElement('button');
+    mainButton.className = "btn main-btn";
+    mainButton.textContent = "Restart";
+    pageEl.appendChild(mainButton);
 }
-
+ var answeredCorrect = function(){
+     numberCorrect++;
+     console.log(numberCorrect);
+ }
+ var answeredWrong = function(){
+     remainingTime-=10;
+ }
 var clickDecision = function(event){
     console.log ("button clicked");
     var targetEl = event.target;
     if (targetEl.matches("#start")){
         displayQuestion(questions[questionNumber]);
         countDown();
+    } else if(targetEl.matches(".main-btn")){
+        mainScreen();
     } else {
         var answerChosen = parseInt(targetEl.getAttribute("data-answer-id"));
     console.log(answerChosen,questions[questionNumber-1].correctAnswer)
         if (answerChosen === questions[questionNumber-1].correctAnswer){
-        console.log("correct");
+        answeredCorrect();
     }else {
-        console.log("wrong");
+        answeredWrong();
     }
     if (questionNumber>=questions.length){
-        endScreen();
+        endScreen(counter);
     } else if (targetEl.matches(".answer")){
     
     displayQuestion(questions[questionNumber]);
@@ -83,12 +105,11 @@ var highScore = function(event){
     pageEl.appendChild(highScoreTitle);
 }
 var countDown = function(){
-var counter = setInterval(function(){
+counter = setInterval(function(){
     timerEl.textContent = remainingTime;
     remainingTime--;
-    if (remainingTime === 0){
+    if (remainingTime < 0){
         endScreen();
-        clearInterval(counter);
     }
 },1000);
 }
