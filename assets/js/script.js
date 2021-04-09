416a139e7d67b238c74bdf139476db9dce806212
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-09 12:20:48 -0500

    question and answer randomization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 var highScoreEl = document.querySelector("#high-score");
 var pageEl = document.querySelector("main");
 var timerEl = document.querySelector("#timer-count");
+var showAnwserMatch = document.createElement("div");
+showAnwserMatch.className = "answer-match";
 var savedHighScores = [];
+var questionOrder = [];
 var questionNumber = 0;
 var remainingTime = 10;
 var numberCorrect = 0;
 var counter = 0;
 var finalScore = 0;
+var answerMatch = "";
 
 var questions = [
   {
@@ -20,43 +24,84 @@ var questions = [
     answers: ["Encapsulation", "Interface", "Composition", "Inheritance"],
     correctAnswer: 3,
   },
+  {
+    question:
+      "Arrays in Javascript can be used to store __________.",
+    answers: ["Numbers and Strings", "Other Arrays", "Booleen", "Objects", "All of the Above"],
+    correctAnswer: 4,
+  },
 ];
-// for (var i = 0; i < questions.length; i++){
-//     questions.number =
-// }
-var sortScores = function () {};
+for (var i = 0; i < questions.length; i++){
+    questionOrder[i] = i;
+}
+
+
 
 var clearMain = function () {
   while (pageEl.firstChild) {
     pageEl.removeChild(pageEl.firstChild);
   }
 };
+
+var randomizer = function(arr){
+    for (var i = 0; i<arr.length; i++){
+        var temp = arr[i];
+        var a = Math.floor(Math.random()*arr.length);
+        arr[i] = arr[a];
+        arr[a] = temp;
+    }
+    return arr;
+}
 var mainScreen = function () {
   clearMain();
   remainingTime = 60;
   questionNumber = 0;
   numberCorrect = 0;
+  answerMatch = "";
+  questionOrder = randomizer(questionOrder);
   var maindisplay = document.createElement("div");
   pageEl.className = "main";
   maindisplay.innerHTML =
     '<h1>Coding Quiz Challenge</h1><p>Try to answer the following questions.</p><p>Once you begin, you will have 60 seconds to complete the quiz.</p><p>Wrong answers will deduct 10 seconds from you time.</p><p>Good Luck!!</p><button class="btn" id="start">Begin Quiz</button>';
   pageEl.appendChild(maindisplay);
 };
+
 var displayQuestion = function (oneQuestion) {
   clearMain();
   pageEl.className = "question-layout";
-  console.log("started");
   var printedQuestion = document.createElement("h1");
   printedQuestion.className = "question";
   printedQuestion.textContent = oneQuestion.question;
   pageEl.appendChild(printedQuestion);
 
+  var answerOrder = [];
+  for (var i = 0; i < oneQuestion.answers.length; i++){
+    answerOrder[i] = i;
+  }
+  answerOrder = randomizer(answerOrder);
+  for (var i = 0; i < oneQuestion.answers.length; i++){
+    if (oneQuestion.answers[answerOrder[i]].toLowerCase()==="all of the above"){
+        console.log(oneQuestion.answers[answerOrder[i]]);
+        console.log(answerOrder,i);
+        console.log(oneQuestion.answers[oneQuestion.answers.length-1]);
+        var temp = answerOrder[i];
+        answerOrder[i] = answerOrder[answerOrder.length-1];
+        answerOrder[answerOrder.length-1] = temp;
+      break;
+    }
+  }
+  
   for (i = 0; i < oneQuestion.answers.length; i++) {
     var answerButton = document.createElement("button");
     answerButton.className = "btn answer";
-    answerButton.textContent = oneQuestion.answers[i];
-    answerButton.setAttribute("data-answer-id", i);
+    answerButton.textContent = oneQuestion.answers[answerOrder[i]];
+    answerButton.setAttribute("data-answer-id", answerOrder[i]);
     pageEl.appendChild(answerButton);
+  }
+  if (!(answerMatch==="")){
+      
+      showAnwserMatch.textContent = answerMatch;
+      pageEl.appendChild(showAnwserMatch);
   }
   questionNumber++;
 };
@@ -65,7 +110,6 @@ var endScreen = function () {
   clearInterval(counter);
   loadScores();
   remainingTime = Math.max(remainingTime, 0);
-  console.log(remainingTime);
   timerEl.textContent = remainingTime;
   clearMain();
   finalScore = 10 * numberCorrect + remainingTime;
@@ -88,6 +132,10 @@ var endScreen = function () {
       savedHighScores[savedHighScores.length - 1].score +
       " to make it to the highscore list </p>";
     pageEl.appendChild(closingTitle);
+    var mainButton = document.createElement("button");
+  mainButton.className = "btn main-btn";
+  mainButton.textContent = "Restart";
+  pageEl.appendChild(mainButton);
   } else {
     closingTitle.innerHTML =
       closingTitle.innerHTML +
@@ -104,19 +152,23 @@ var endScreen = function () {
     highScoreForm.appendChild(highScoreSubmit);
     pageEl.appendChild(highScoreForm);
   }
-  var mainButton = document.createElement("button");
-  mainButton.className = "btn main-btn";
-  mainButton.textContent = "Restart";
-  pageEl.appendChild(mainButton);
+  showAnwserMatch.textContent = answerMatch;
+  pageEl.appendChild(showAnwserMatch);
+  
 };
 
 var answeredCorrect = function () {
   numberCorrect++;
-  console.log(numberCorrect);
+  var correctSound = new Audio("./assets/mp3/correct-answer.mp3");
+  correctSound.play();
+  answerMatch = "Correct!";
 };
 
 var answeredWrong = function () {
   remainingTime -= 10;
+  var wrongSound = new Audio("./assets/mp3/wrong-answer.mp3");
+  wrongSound.play();
+  answerMatch = "Wrong!";
 };
 
 var loadScores = function () {
@@ -128,28 +180,25 @@ var loadScores = function () {
 };
 
 var clickDecision = function (event) {
-  console.log("button clicked");
   var targetEl = event.target;
   if (targetEl.matches("#start")) {
-    displayQuestion(questions[questionNumber]);
+    displayQuestion(questions[questionOrder[questionNumber]]);
     countDown();
   } else if (targetEl.matches(".main-btn")) {
     mainScreen();
   } else if (targetEl.matches(".answer")) {
     var answerChosen = parseInt(targetEl.getAttribute("data-answer-id"));
-    console.log(answerChosen, questions[questionNumber - 1].correctAnswer);
-    if (answerChosen === questions[questionNumber - 1].correctAnswer) {
+    if (answerChosen === questions[questionOrder[questionNumber-1]].correctAnswer) {
       answeredCorrect();
     } else {
       answeredWrong();
     }
     if (questionNumber >= questions.length) {
-      endScreen(counter);
+      endScreen();
     } else {
-      displayQuestion(questions[questionNumber]);
+      displayQuestion(questions[questionOrder[questionNumber]]);
     }
   } else if (targetEl.matches("#submit-btn")) {
-    console.log(document.querySelector("input[name='initials']").value);
     var highScoreObj = {
       name: document.querySelector("input[name='initials']").value,
       score: finalScore,
@@ -161,6 +210,10 @@ var clickDecision = function (event) {
     });
     localStorage.setItem("scores", JSON.stringify(savedHighScores));
     highScore();
+  } else if (targetEl.matches(".reset")){
+      localStorage.clear();
+      savedHighScores = "";
+      highScore();
   }
 };
 
@@ -185,10 +238,17 @@ var highScore = function (event) {
     scoreList.appendChild(scoreListScore);
     pageEl.appendChild(scoreList);
   }
+  var buttonsEl = document.createElement("div");
+  buttonsEl.className = "hs-buttons";
   var mainButton = document.createElement("button");
   mainButton.className = "btn main-btn";
   mainButton.textContent = "Return Home";
-  pageEl.appendChild(mainButton);
+  buttonsEl.appendChild(mainButton);
+  var resetButton = document.createElement("button");
+  resetButton.className = "btn reset";
+  resetButton.textContent = "Clear High Scores";
+  buttonsEl.appendChild(resetButton);
+  pageEl.appendChild(buttonsEl);
 };
 
 var countDown = function () {
